4440a4dfca67181225e2d84128553f11e1ca6b7e
Jean-Paul Lacy <lacyjpr@gmail.com>
2017-07-27 19:17:18 -0700

    Write banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const Banner = () => (
-  <div>
-    <h2>Banner Component</h2>
+  <div className="banner">
+    <a href="https://www.freecodecamp.com"><img className="logo" src="https://www.freecodecamp.com/design-style-guide/img/freeCodeCamp.svg" alt="freeCodeCamp logo"/></a>
   </div>
 );
 
